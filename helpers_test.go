47cb9c9da2c767c8995d5d4b331af51575730978
Taylor Wiebe <twiebe@vendasta.com>
2017-11-15 17:01:25 -0600

    add helper test

--- helpers_test.go
+++ helpers_test.go
@@ -12,19 +12,30 @@ func TestToTime(t *testing.T) {
 		name          string
 		in            string
 		expected      time.Time
-		expectedError error
-	}{}
+		expectedError bool
+	}{
+		{
+			name:          "valid time string",
+			in:            "Thu, 31 Aug 2017 01:10:56 +0000",
+			expected:      time.Date(2017, time.August, 31, 1, 10, 56, 0, time.UTC),
+			expectedError: false,
+		},
+		{
+			name:          "invalid time",
+			in:            "August 31 2017 01:10:56 +0000",
+			expected:      time.Time{},
+			expectedError: true,
+		},
+	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			actual, err := ToTime(tt.in)
 			assert.Equal(t, tt.expected, actual)
-			if err != nil && tt.expectedError == nil {
+			if err != nil && !tt.expectedError {
 				t.Errorf("did not expect an error, got: %v", err)
-			} else if err == nil && tt.expectedError != nil {
+			} else if err == nil && tt.expectedError {
 				t.Error("expected an error, got nil")
-			} else {
-				assert.Equal(t, tt.expectedError, err)
 			}
 		})
 	}
